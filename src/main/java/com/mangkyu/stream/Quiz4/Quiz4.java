a9f19be4c20df017f182c6357aa43181ce4c262b
n4oah <n4oahdev@gmail.com>
2023-04-10 23:04:07 +0900

    쓸개없는 import 제거

--- src/main/java/com/mangkyu/stream/Quiz4/Quiz4.java
+++ src/main/java/com/mangkyu/stream/Quiz4/Quiz4.java
@@ -2,7 +2,6 @@ package com.mangkyu.stream.Quiz4;
 
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
